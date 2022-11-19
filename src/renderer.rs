dfe345c253d44fae31897d82d81a04806674dd0f
Julian Andrews <jandrews271@gmail.com>
2022-11-19 17:57:49 -0500

    Fix clippy complaints

--- src/renderer.rs
+++ src/renderer.rs
@@ -3,20 +3,14 @@ use std::sync::{Arc, Mutex};
 use anyhow::Result;
 use once_cell::sync::Lazy;
 
-pub static RENDERER: Lazy<Renderer> = Lazy::new(Renderer::new);
+pub static RENDERER: Lazy<Renderer> = Lazy::new(Renderer::default);
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Default)]
 pub struct Renderer {
     tera: Arc<Mutex<tera::Tera>>,
 }
 
 impl Renderer {
-    pub fn new() -> Self {
-        Self {
-            tera: Arc::new(Mutex::new(tera::Tera::default())),
-        }
-    }
-
     pub fn add_template(&self, name: &str, template: &str) -> Result<()> {
         self.tera.lock().unwrap().add_raw_template(name, template)?;
 
